e6460b2f54bc9931c5091af48237a5521dbfd00a
Scott Wu <sw@scottwu.ca>
2025-09-01 01:13:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { resolveCommand } from 'package-manager-detector/commands'
+import { detect } from 'package-manager-detector/detect'
+import { spawnSync } from 'child_process'
+
+const [, , ...args2] = process.argv;
+
+if (args2.length === 0) {
+  console.error("Usage: run-vite <command> [args...]");
+  process.exit(1);
+}
+
+const pm = await detect()
+if (!pm) throw new Error('Could not detect package manager')
+
+const { command, args } = resolveCommand(
+  pm.agent,
+  'execute',
+  [
+    "vite-node",
+    "--options.transformMode.ssr=/.*/",
+    ...args2]
+  )
+
+console.log(`Detected the ${pm.agent} package manager.
+  
+  running "${command}" ${args.join(' ')}`)
+
+const result = spawnSync(command, args, { stdio: "inherit" });
+
+process.exit(result.status);
